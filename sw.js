0226a310d11325d3753e4972cd6ca696a0357f86
Suzi577 <susandell1690@gmail.com>
2026-08-15 23:37:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ecchos-of-the-past-cache-v3";
+const CACHE_NAME = "echoes-pwa-v-fix3";
 const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
